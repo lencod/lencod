de09e24bc07787453c4ee6517c58eea76f18fe28
lencod <ooocyber65@proton.me>
2025-10-31 03:33:09 +1100

    Update deepseek_powershell_20251030_fb481f.ps1

--- deepseek_powershell_20251030_fb481f.ps1
+++ deepseek_powershell_20251030_fb481f.ps1
@@ -1,1 +1,8 @@
-Start-Process -FilePath "notepad.exe" -WindowStyle Hidden
+# test_notepad.ps1
+Start-Process notepad.exe
+
+# Принудительная задержка чтобы убедиться что скрипт выполнился
+Start-Sleep -Seconds 3
+
+# Запись в лог для подтверждения выполнения
+"PowerShell script executed successfully at $(Get-Date)" | Out-File "$env:TEMP\ps_script.log" -Append
